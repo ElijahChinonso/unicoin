9b8b26262e94f35223a9ec4eee8bd0a30229d56f
ElijahChinonso <okougbeninelijah003@gmail.com>
2023-01-22 23:19:18 +0100

    created the white divs and the Icon for the logIn page.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import 'animate.css'
 import { Routes, Route } from 'react-router-dom';
 import Logo from "./pages/home/component/logo";
 import SignUp from "./pages/sign_up/component/signUp";
+import LogIn from "./pages/log_in/component/logIn";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<Logo/>}/>
+          <Route path={"/"} element={<Logo/>}/>
           <Route path={"/signUp"} element={<SignUp/>}/>
+          <Route path={"/logIn"} element={<LogIn/>}/>
       </Routes>
 
     </div>
