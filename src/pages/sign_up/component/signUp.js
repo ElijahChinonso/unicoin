84942b8d218c69b196ef9150e55606b57b6693be
ElijahChinonso <okougbeninelijah003@gmail.com>
2023-01-22 22:54:25 +0100

    created the logIn button.

--- src/pages/sign_up/component/signUp.js
+++ src/pages/sign_up/component/signUp.js
@@ -2,12 +2,14 @@ import React from "react";
 import '../styles/signUp.css'
 import LogoDivs from "./logoDivs";
 import Form from '../../sign_up/component/form'
+import LogInBtn from "./logIn_btn";
 
 export default function SignUp () {
     return(
         <div className={'signUp_page'}>
             <LogoDivs/>
             <Form/>
+            <LogInBtn/>
         </div>
     )
 }
